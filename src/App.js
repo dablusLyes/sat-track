68cb18f4c91349273044c6af3313ba6b6390ff4a
Lyes <lyes.dabla@gmail.com>
2022-01-12 01:46:33 +0100

    lll

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Map from './components/Map'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-title">
+        <h1>Satellite tracker</h1>
+      </div>
+      <div className="leaflet-container">
+        <Map/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
